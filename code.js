72ecf1e8a7f1e53634f3f3234e4ca093ad880105
Johan <johanhah@kth.se>
2023-01-08 18:12:43 +0100

    fixed error

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 
 function myFunction(){
-const codes = ["9146e7", "cf4ac3", "f4bf19"]
+    const codes = ["9146e7", "cf4ac3", "f4bf19"]
     let code = document.getElementById('input-code').value
     if(codes.includes(code)){
-        window.location.href("folder/" + code + ".html")
+        window.location.href = "folder/" + code + ".html"
     }
 };
